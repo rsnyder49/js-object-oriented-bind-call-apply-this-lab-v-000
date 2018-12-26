bb400e0d3ef7f0d9b49c3aae19094868e8868f93
Robert Snyder <r.snyder49@gmail.com>
2018-12-26 19:45:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg);
 }
 
-function setThisWithApply(fn, thisValue) {
-  fn.apply()
+function setThisWithApply(fn, thisValue, args) {
+  fn.apply(thisValue, args);
 }
